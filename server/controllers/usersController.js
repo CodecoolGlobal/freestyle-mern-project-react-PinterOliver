2f7e1f4c9c929580cfd55a2277bae11c4a23e00f
Bebe <125598346+SebestyenBenedek@users.noreply.github.com>
2023-06-26 22:17:53 +0200

    add deleteOneUser function

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -26,6 +26,21 @@ const getOneUser = async (req, res) => {
     }
   };
 
+//CREATE a new user (registration)
+
+//DELETE one user
+const deleteOneUser = async (req, res) => {
+    try {
+      const { id } = req.params
+      const user = await User.findOneAndDelete({_id: id})
+      if (!user) {
+          return res.status(404).json({error: 'No such book'})
+      }
+      res.status(200).json(user)
+    } catch (error) {
+      res.status(400).json({error: error.message});
+    }
+  };
 
 module.exports = {
   getAllUsers,
