5a1ff68fc9178442fb3b9509d76fca41f125be86
Oliver Peter Pinter <125395118+PinterOliver@users.noreply.github.com>
2023-06-26 15:05:10 +0200

    add models to api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,12 @@
-import Something from '../model/Something.js';
-import express from 'express';
+const Book = require('../model/Book.js');
+const OrderHeader = require('../model/OrderHeader.js');
+const OrderItem = require('../model/OrderItem.js');
+const Role = require('../model/Role.js');
+const StoredItem = require('../model/StoredItem.js');
+const User = require('../model/User.js');
+const express = require('express');
 const router = express.Router();
 
-console.log(Something);
+console.log([Book, OrderHeader, OrderItem, Role, StoredItem, User]);
 
 export default router;
