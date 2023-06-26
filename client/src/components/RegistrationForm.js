1e3025aab1bad22ed8ead0d0395cd61e4e647ef2
Bebe <125598346+SebestyenBenedek@users.noreply.github.com>
2023-06-26 23:22:23 +0200

    add handler to registrationform inputs and button

--- client/src/components/RegistrationForm.js
+++ client/src/components/RegistrationForm.js
@@ -1,39 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 function RegistrationForm() {
+
+  const [userName, setUserName] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleInputChange = (e) => {
+    const {id, value} = e.target;
+    if (id === 'userName') setUserName(value);
+    if (id === 'firstName') setFirstName(value);
+    if (id === 'lastName') setLastName(value);
+    if (id === 'email') setEmail(value);
+    if (id === 'password') setPassword(value);
+  };
+
+  const handleSubmit = () => {
+    console.log(userName, firstName, lastName, email, password);
+  };
+
   return (
     <div className="form">
       <div className="form-body">
         <div className="userName">
-          <label className="form__label" htmlFor="userName">Username </label>
-          <input className="form__input" type="text" id="userName" placeholder="Username"/>
+          <label className="register_label">Username </label>
+          <input className="register_input" type="text" id="userName" placeholder="Username"
+            value={userName} onChange={(e) => handleInputChange(e)}/>
         </div>
         <div className="firstName">
-          <label className="form__label" htmlFor="firstName">First Name </label>
-          <input  type="text" name="" id="firstName"  className="form__input"placeholder="Firstname"/>
+          <label className="register_label">First Name </label>
+          <input className="register_input" type="text" id="firstName" placeholder="Firstname"
+            value={firstName} onChange={(e) => handleInputChange(e)}/>
         </div>
         <div className="lastName">
-          <label className="form__label" htmlFor="lastName">Last Name </label>
-          <input  type="text" name="" id="lastName"  className="form__input"placeholder="Lastname"/>
+          <label className="register_label">Last Name </label>
+          <input className="register_input" type="text" id="lastName" placeholder="Lastname"
+            value={lastName} onChange={(e) => handleInputChange(e)}/>
         </div>
         <div className="email">
-          <label className="form__label" htmlFor="email">Email </label>
-          <input  type="email" id="email" className="form__input" placeholder="Email"/>
+          <label className="register_label">Email </label>
+          <input className="register_input" type="email" id="email" placeholder="Email"
+            value={email} onChange={(e) => handleInputChange(e)}/>
         </div>
         <div className="password">
-          <label className="form__label" htmlFor="password">Password </label>
-          <input className="form__input" type="password"  id="password" placeholder="Password"/>
-        </div>
-        <div className="confirm-password">
-          <label className="form__label" htmlFor="confirmPassword">Confirm Password </label>
-          <input className="form__input" type="password" id="confirmPassword" placeholder="Confirm Password"/>
+          <label className="register_label">Password </label>
+          <input className="register_input" type="password" id="password" placeholder="Password"
+            value={password} onChange={(e) => handleInputChange(e)}/>
         </div>
       </div>
       <div className="registerSubmit">
-        <button type="submit" className="btn">Register</button>
+        <button onClick={() => handleSubmit()} type="submit" className="btn">Register</button>
       </div>
     </div>
   );
 }
+
 export default RegistrationForm;
