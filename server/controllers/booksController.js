7d5baa6307b33bc0e4fb3206381523df0cb8d628
Bebe <125598346+SebestyenBenedek@users.noreply.github.com>
2023-06-26 21:24:19 +0200

    add deleteOneBook function

--- server/controllers/booksController.js
+++ server/controllers/booksController.js
@@ -40,6 +40,20 @@ const addOneBook = async (req, res) => {
   }
 };
 
+//DELETE a book with ADMIN account
+const deleteOneBook = async (req, res) => {
+  try {
+    const { id } = req.params
+    const book = await Book.findOneAndDelete({_id: id})
+    if (!book) {
+        return res.status(404).json({error: 'No such todo'})
+    }
+    res.status(200).json(book)
+  } catch (error) {
+    res.status(400).json({error: error.message});
+  }
+}
+
 module.exports = {
   getAllBooks,
   getOneBook,
