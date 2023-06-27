1fb378180f2baabcfaa9654927664ef627e3591e
Daniel-Toth <danthe34@gmail.com>
2023-06-27 19:48:09 +0200

    add style and filtering slider to bookFilter page

--- client/src/components/BookFilter/BookFilter.jsx
+++ client/src/components/BookFilter/BookFilter.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import './BookFilter.css';
+import { useState } from "react";
+import "./BookFilter.css";
 
 const BookFilter = () => {
-  return (
-    <div>BookFilter</div>
-  )
-}
 
-export default BookFilter
+  const [sliderValue, setSliderValue] = useState(50);
+
+  const handleSliderChange = (event) => {
+    setSliderValue(event.target.value);
+  };
+
+  const sliderStyles = {
+    background: `linear-gradient(to right, #85bb65 0%, #85bb65 ${sliderValue}%, #d3d3d3 ${sliderValue}%, #d3d3d3 100%)`
+  };
+
+  return (
+    <div className="filterContainer">
+      <div className="slidecontainer">
+        <div className="sliderText">Price range: 1-{sliderValue}€</div>
+        <input
+          type="range"
+          min="1"
+          max="100"
+          value={sliderValue}
+          className="slider"
+          id="myRange"
+          onChange={handleSliderChange}
+          style={sliderStyles}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default BookFilter;
--- client/src/components/BookItem/BookItem.jsx
+++ client/src/components/BookItem/BookItem.jsx
@@ -3,10 +3,12 @@ import './BookItem.css';
 
 const BookItem = (props) => {
   return (
+    <div className='itemsContainer'>
     <div className='bookContainer'>
       <img className="bookThumbnail" src={props.books.img}/>
       <h2 className="bookTitle">{props.books.title}</h2>
-      <p className="bookPrice">{props.books.price}</p>
+      <p className="bookPrice">{props.books.price}€</p>
+    </div>
     </div>
   )
 }
