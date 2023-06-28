a54c8b4844e06734794c19f47907d83d03b50829
Bebe <125598346+SebestyenBenedek@users.noreply.github.com>
2023-06-28 22:14:00 +0200

    fix OrdersTable.jsx file

---
+++ client/src/components/OrdersTable/OrdersTable.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './OrdersTable.css';
+
+function BooksTable({ orderList, onDelete }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Author</th>
+          <th>Price</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {orderList.map((order) => (
+          <tr key={order._id}>
+            <td>{order.totalPrice}</td>
+            <td>{order.state}</td>
+            <td>{order.createdAt}</td>
+            <td>
+              <button>Update</button>
+              <button onClick={() => onDelete(order._id)}>Delete</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default BooksTable;
