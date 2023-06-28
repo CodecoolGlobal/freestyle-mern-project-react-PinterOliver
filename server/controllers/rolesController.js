44b26ef20c7d5b81950b2141d7179c9d66c88e0b
Oliver Peter Pinter <125395118+PinterOliver@users.noreply.github.com>
2023-06-28 15:58:52 +0200

    refactor with eslint

--- server/controllers/rolesController.js
+++ server/controllers/rolesController.js
@@ -1,29 +1,28 @@
 /* eslint-disable consistent-return */
-const User = require('../model/User');
 const Role = require('../model/Role');
 
 // GET all roles
 const getAllRoles = async (req, res) => {
-    try {
-      const roles = await Role.find({}).sort({name: 1});
-      res.status(200).json(roles);
-    } catch (error) {
-      res.status(400).json({error: error.message});
-    }
+  try {
+    const roles = await Role.find({}).sort({name: 1});
+    res.status(200).json(roles);
+  } catch (error) {
+    res.status(400).json({error: error.message});
+  }
 };
 
 //GET one role
 const getOneRole = (req, res) => {
-    try {
-      const role = req.roleData;
-      res.status(200).json(role);
-    } catch (error) {
-      res.status(400).json({error: error.message});
-    }
+  try {
+    const role = req.roleData;
+    res.status(200).json(role);
+  } catch (error) {
+    res.status(400).json({error: error.message});
+  }
 };
 
 
 module.exports = {
-    getAllRoles,
-    getOneRole
-  };
+  getAllRoles,
+  getOneRole,
+};
