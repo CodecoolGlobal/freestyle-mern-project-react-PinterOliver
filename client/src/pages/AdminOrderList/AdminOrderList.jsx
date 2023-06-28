0e7d836a46aee68407f8db4d3a0d324a18a57cf1
Bebe <125598346+SebestyenBenedek@users.noreply.github.com>
2023-06-28 22:04:11 +0200

    add AdminOrderList.jsx to AdminOrderList pages

---
+++ client/src/pages/AdminOrderList/AdminOrderList.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import './AdminOrderList.css';
+//import BooksTable from '../../components/BooksTable';
+import Loading from '../../components/Loading';
+
+function AdminOrderList() {
+  const [loading, setLoading] = useState(true);
+  const [orderList, setOrderList] = useState([]);
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      const response = await fetch('/api/orders');
+      const jsonData = await response.json();
+      setOrderList(jsonData.orders);
+      setLoading(false);
+    };
+    fetchOrders();
+  }, []);
+
+  const handleDelete = async (id) => {
+    const response = await fetch(`/api/oders/${id}`, {
+      method: 'DELETE',
+    });
+    console.log(await response.json());
+    setOrderList(orderList.filter((order) => order._id !== id));
+  };
+
+  if (loading) return <Loading />;
+
+  return <OrdersTable orderList={orderList} onDelete={handleDelete} />;
+}
+
+export default AdminOrderList;
