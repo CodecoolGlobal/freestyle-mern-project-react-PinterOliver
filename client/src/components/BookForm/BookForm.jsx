0dadcf93ffe10c351b0976a45900b5b4116380e9
Tamas Molnar <125677315+Tomocza@users.noreply.github.com>
2023-06-28 18:28:12 +0200

    add book updating on admin side

--- client/src/components/BookForm/BookForm.jsx
+++ client/src/components/BookForm/BookForm.jsx
@@ -1,9 +1,103 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function BookForm() {
+function BookForm({ book, onCancel, onSave }) {
+  const [title, setTitle] = useState(book?.title ?? '');
+  const [author, setAuthor] = useState(book?.author ?? '');
+  const [year, setYear] = useState(book?.publishedYear ?? '');
+  const [price, setPrice] = useState(book?.price ?? '');
+  const [genres, setGenres] = useState(book?.genres ?? '');
+  const [description, setDescription] = useState(book?.description ?? '');
+  const [url, setUrl] = useState(book?.image_url ?? '');
+
+  const onSubmit = () => {
+    if (book) {
+      onSave({
+        ...book,
+        title,
+        author,
+        publishedYear: year,
+        price,
+        genres,
+        description,
+        image_url: url,
+      });
+    }
+
+    onSave({
+      title,
+      author,
+      publishedYear: year,
+      price,
+      genres,
+      description,
+      image_url: url,
+    });
+  };
+
   return (
-    <div>BookForm</div>
-  )
+    <form onSubmit={onSubmit}>
+      <div className="control">
+        <label htmlFor="title">Title:</label>
+        <input value={title} onChange={(e) => setTitle(e.target.value)} name="title" id="title" />
+      </div>
+
+      <div className="control">
+        <label htmlFor="author">Author:</label>
+        <input
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+          name="author"
+          id="author"
+        />
+      </div>
+
+      <div className="control">
+        <label htmlFor="year">Year:</label>
+        <input value={year} onChange={(e) => setYear(e.target.value)} name="year" id="year" />
+      </div>
+
+      <div className="control">
+        <label htmlFor="price">Price:</label>
+        <input
+          type="number"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+          name="price"
+          id="price"
+        />
+      </div>
+
+      <div className="control">
+        <label htmlFor="genres">Genres:</label>
+        <input
+          value={genres}
+          onChange={(e) => setGenres(e.target.value)}
+          name="genres"
+          id="genres"
+        />
+      </div>
+
+      <div className="control">
+        <label htmlFor="description">Description:</label>
+        <input
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          name="description"
+          id="description"
+        />
+      </div>
+
+      <div className="control">
+        <label htmlFor="url">url:</label>
+        <input value={url} onChange={(e) => setUrl(e.target.value)} name="url" id="url" />
+      </div>
+
+      <div className="buttons">
+        <button type="submit">{book ? 'Update book' : 'Create book'}</button>
+        <button onClick={onCancel}>Cancel</button>
+      </div>
+    </form>
+  );
 }
 
-export default BookForm
+export default BookForm;
--- client/src/pages/AdminBookUpdater/AdminBookUpdater.jsx
+++ client/src/pages/AdminBookUpdater/AdminBookUpdater.jsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BookForm from '../../components/BookForm/BookForm';
+import { useNavigate, useParams } from 'react-router';
+import Loading from '../../components/Loading';
 
 function AdminBookUpdater() {
-  return <BookForm />;
+  const [book, setBook] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchBook = async () => {
+      setLoading(true);
+      const response = await fetch(`/api/books/${id}`);
+      const jsonData = await response.json();
+      setBook(jsonData.book);
+      setLoading(false);
+    };
+    fetchBook();
+  }, [id]);
+
+  const handleSave = async (book) => {
+    setLoading(true);
+    await fetch(`/api/books/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-type': 'application/json',
+        token: localStorage.getItem('token'),
+      },
+      body: JSON.stringify(book),
+    });
+    setLoading(false);
+    navigate('/admin/books');
+  };
+
+  if (loading) return <Loading />;
+
+  return <BookForm book={book} onCancel={() => navigate('/admin/books')} onSave={handleSave} />;
 }
 
 export default AdminBookUpdater;
