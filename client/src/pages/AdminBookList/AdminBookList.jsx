69e65cd676bac9f4094d0e1df601021fc81a4bca
Tamas Molnar <125677315+Tomocza@users.noreply.github.com>
2023-06-28 12:45:05 +0200

    add loading

--- client/src/pages/AdminBookList/AdminBookList.jsx
+++ client/src/pages/AdminBookList/AdminBookList.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import './AdminBookList.css';
 import BooksTable from '../../components/BooksTable';
+import Loading from '../../components/Loading';
 
 function AdminBookList() {
+  const [loading, setLoading] = useState(true);
   const [bookList, setBookList] = useState([]);
 
   useEffect(() => {
@@ -10,9 +12,12 @@ function AdminBookList() {
       const response = await fetch('/api/books');
       const jsonData = await response.json();
       setBookList(jsonData.books);
+      setLoading(false);
     };
     fetchBooks();
   }, []);
+
+  if (loading) return <Loading />;
 
   return <BooksTable bookList={bookList} />;
 }
