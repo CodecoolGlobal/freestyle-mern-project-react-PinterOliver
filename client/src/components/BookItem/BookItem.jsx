83fae6ebf80b6865d36fc2340527a588062f65a8
Daniel-Toth <danthe34@gmail.com>
2023-06-28 16:35:06 +0200

    make all 'add to cart' buttons function

--- client/src/components/BookItem/BookItem.jsx
+++ client/src/components/BookItem/BookItem.jsx
@@ -1,7 +1,56 @@
-import React from "react";
+import { useState } from "react";
 import "./BookItem.css";
 
 const BookItem = (props) => {
+
+  function checkLocalStorageCart() {
+    const cart = localStorage.getItem('cart');
+    const hasCart = cart !== null && cart !== undefined;
+    if (hasCart){
+      addToCart(props.book);
+    }
+    else {
+      const myObject =  {
+        [props.book._id]: {
+          title: props.book.title,
+          price: props.book.price,
+          amount: 1,
+        },
+      
+      };
+      // Convert the JSON object to a string
+      const jsonString = JSON.stringify(myObject);
+      // Save the stringified JSON object to local storage
+      localStorage.setItem("cart", jsonString);
+    }
+  }
+
+  const [book, setBook] = useState(props.book);
+
+  function addToCart( book ) {
+    const storedJsonString = localStorage.getItem("cart");
+    const storedObject = JSON.parse(storedJsonString);
+
+    if (storedObject[book._id]) {
+      storedObject[book._id].amount++;
+      const jsonString = JSON.stringify(storedObject);
+      localStorage.setItem("cart", jsonString);
+    } else {
+
+      storedObject[book._id] = {
+        title: book.title,
+        price: book.price,
+        amount: 1,
+      };
+      // Convert the JSON object to a string
+      const jsonString = JSON.stringify(storedObject);
+      // Save the stringified JSON object to local storage
+      localStorage.setItem("cart", jsonString);
+    }
+
+    return null;
+  }
+
   const maxLength = 20;
   const limitedTitle =
     props.book.title.length > maxLength
@@ -16,7 +65,7 @@ const BookItem = (props) => {
           <h2 className="bookTitle">{limitedTitle}</h2>
         </a>
         <p className="bookPrice">{props.book.price}€</p>
-        <button>Add to cart</button>
+        <button onClick={() => checkLocalStorageCart()}>Add to cart</button>
       </div>
     </div>
   );
