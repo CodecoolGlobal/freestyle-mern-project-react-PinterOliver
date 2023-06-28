062c0e16e495a1331b1585f69b2c2fc1b3b614b1
Tamas Molnar <125677315+Tomocza@users.noreply.github.com>
2023-06-28 12:41:37 +0200

    fix scrolling issue

--- client/src/pages/Layout/Layout.jsx
+++ client/src/pages/Layout/Layout.jsx
@@ -5,19 +5,19 @@ import { Outlet, Link } from 'react-router-dom';
 function Layout() {
   return (
     <div className="Layout">
-      <nav className="main-navbar">
-        <ul>
-          <li>
-            <img className="main-logo" src="/logo.png" alt="main logo"></img>
-          </li>
-          <li className="grow">Books</li>
-          <li>
-            <Link to="/admin">Admin</Link>
-          </li>
-          <li>Cart</li>
-        </ul>
-      </nav>
-      <div>
+      <div className="main-navbar">
+        <img className="main-logo" src="/logo.png" alt="main logo"></img>
+        <a href="/books">
+          <button>Books</button>
+        </a>
+        <a href="/admin">
+          <button>Admin</button>
+        </a>
+        <a href="/cart">
+          <button>Cart</button>
+        </a>
+      </div>
+      <div className="main-content">
         <Outlet />
       </div>
     </div>
