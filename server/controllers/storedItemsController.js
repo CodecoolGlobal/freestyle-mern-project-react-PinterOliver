c9d2f354627b1db7816a862b5726b2407e355513
Oliver Peter Pinter <125395118+PinterOliver@users.noreply.github.com>
2023-06-29 00:54:14 +0200

    add getAllStoredItems

--- server/controllers/storedItemsController.js
+++ server/controllers/storedItemsController.js
@@ -1,44 +1,30 @@
 /* eslint-disable require-atomic-updates */
 /* eslint-disable consistent-return */
-const Book = require('../model/Book');
 const StoredItem = require('../model/StoredItem');
 
 const {
-  stringSearch,
   numberSearch,
-  arraySearch,
   toSort,
 } = require('./filterAndSort');
 
 // GET all stored items
 const getAllStoredItems = async (req, res) => {
   try {
-    const { title, author, maxprice, genres, sort } = req.query;
+    const { maxamount, sort } = req.query;
     let search = {};
-    if (title) search = stringSearch(search, 'title', title);
-    if (author) search = stringSearch(search, 'author', author);
-    if (maxprice) {
-      const numberMaxprice = Number(maxprice);
-      search = numberSearch(search, 'price', numberMaxprice, 'lte');
-    }
-    if (genres) {
-      const genresArray = genres.split(',');
-      search = arraySearch(search, 'genres', genresArray);
+    if (maxamount) {
+      const numberMaxamount = Number(maxamount);
+      search = numberSearch(search, 'amount', numberMaxamount, 'lte');
     }
     let sortBy = {
-      'title': 1,
+      'amount': 1,
     };
     if (sort) {
       const [type, ascend] = sort.split(',');
       sortBy = toSort(sortBy, type, ascend);
     }
-    const books = await Book.find(search).sort(sortBy);
-    const fullBooks = await Promise.all(books.map(async (book) => {
-      const amount = await StoredItem.find({item: book._id});
-      book.amount = amount;
-      return book;
-    }));
-    res.status(200).json({storeditems: fullBooks});
+    const storedItems = await StoredItem.find(search).sort(sortBy).populate('item');
+    res.status(200).json({storeditems: storedItems});
   } catch (error) {
     res.status(400).json({error: error.message});
   }
