de5f878bc453d09ffda2945983b76c1d0a8b200c
Daniel-Toth <danthe34@gmail.com>
2023-06-29 14:00:29 +0200

    add loading

--- client/src/pages/BookPage/BookPage.js
+++ client/src/pages/BookPage/BookPage.js
@@ -2,34 +2,31 @@ import { useState, useEffect } from "react";
 import BookItem from "../../components/BookItem/BookItem";
 import BookFilter from "../../components/BookFilter/BookFilter";
 import './BookPage.css';
+import Loading from "../../components/Loading";
 
 const BookPage = () => {
   const [books, setBooks] = useState([]);
   const [maxPrice, setMaxPrice] = useState(50)
   const [sort, setSort] = useState('title,ascend')
-
-/*   useEffect(() => {
-    fetch("/api/books")
-      .then((res) => res.json())
-      .then((data) => {
-        setBooks(data.books);
-      });
-  }, []); */
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch(`/api/books?maxprice=${maxPrice}&sort=${sort}`)
       .then((res) => res.json())
       .then((data) => {
         setBooks(data.books);
+        setLoading(false)
       });
   }, [maxPrice, sort])
 
+  if (loading) return <Loading />
   return (
     <div className="items">
       <BookFilter OnFilter={(value)=> setMaxPrice(value)} OnSort={(value) => setSort(value)} />
       {books?.map((book) => (
         <BookItem book={book} />
       ))}
+    
     </div>
   );
 };
