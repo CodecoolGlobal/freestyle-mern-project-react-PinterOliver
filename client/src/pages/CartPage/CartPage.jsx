751cf9bdfde30f4c40182864ec4634445af8757c
Oliver Peter Pinter <125395118+PinterOliver@users.noreply.github.com>
2023-06-30 02:48:20 +0200

    add PATCH fetch to cart inputs in CartPage

--- client/src/pages/CartPage/CartPage.jsx
+++ client/src/pages/CartPage/CartPage.jsx
@@ -4,9 +4,44 @@ import CartTable from '../../components/CartTable';
 function CartPage() {
   const [cart, setCart] = useState(JSON.parse(localStorage.getItem('cart')));
 
-  const handleCartUpdate = (newCart) => {
+  const handleCartUpdate = async (newCart) => {
     setCart(structuredClone(newCart));
+    const cartid = localStorage.getItem('cartid');
+    const token = localStorage.getItem('token');
     localStorage.setItem('cart', JSON.stringify(newCart));
+    const jsonItems = await Promise.all(newCart.map(async (item) => {
+      const smallData = await fetch(`/api/orderitems/orderheaders/${cartid}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          token: token,
+        },
+        body: JSON.stringify({
+          bookid: item.id,
+          amount: Number(item.amount),
+        }),
+      });
+      const smallJSON = await smallData.json();
+      if (smallData.status !== 201) {
+        if (smallJSON.rightMethod) {
+          const otherData = await fetch(`/api/orderitems/${smallJSON.orderItem._id}`, {
+            method: smallJSON.rightMethod,
+            headers: {
+              'Content-Type': 'application/json',
+              token: token,
+            },
+            body: JSON.stringify({
+              bookid: item.id,
+              amount: Number(item.amount),
+            }),
+          });
+          const otherJSON = await otherData.json();
+          smallJSON.plus = otherJSON;
+        } else console.log(smallJSON.error);
+      }
+      return smallJSON;
+    }));
+    console.log(jsonItems);
   };
 
   return <CartTable cart={cart} onUpdate={handleCartUpdate} />;
