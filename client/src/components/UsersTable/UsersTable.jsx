eda7ae555806dfcf353c54ce824eeb44f7c2310f
Tamas Molnar <125677315+Tomocza@users.noreply.github.com>
2023-07-24 15:03:48 +0200

    add style to userstable

--- client/src/components/UsersTable/UsersTable.jsx
+++ client/src/components/UsersTable/UsersTable.jsx
@@ -1,42 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './UsersTable.css';
 
 function UsersTable({ userList, onDelete }) {
+  const [expandedRow, setExpandedRow] = useState('');
+
   return (
     <table>
       <thead>
         <tr>
-          <th>Username</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Role</th>
-          <th>Email</th>
-          <th>Delivery address</th>
-          <th>Tel. Num.</th>
-          <th></th>
+          <th className="userNameCol">Username</th>
+          <th className="firstNameCol">First Name</th>
+          <th className="lastNameCol">Last Name</th>
+          <th className="roleCol">Role</th>
+          <th className="emailCol">Email</th>
+          <th className="addrCol">Delivery address</th>
+          <th className="telCol">Tel. Num.</th>
+          <th className="btnCol"></th>
         </tr>
       </thead>
       <tbody>
         {userList.map((user) => (
           <tr key={user._id}>
-            <td>{user.userName}</td>
-            <td>{user.name.first}</td>
-            <td>{user.name.last}</td>
-            <td>{user.role.name}</td>
-            <td>{user.email}</td>
-            <td className='multiline'>
-              {user.delivery?.country}
-              {' '}
-              {user.delivery?.post_code}
+            <td className="userNameCol">{user.userName}</td>
+            <td className="firstNameCol">{user.name.first}</td>
+            <td className="lastNameCol">{user.name.last}</td>
+            <td className="roleCol">{user.role.name}</td>
+            <td className="emailCol">{user.email}</td>
+            <td className="addrCol multiline">
+              {user.delivery?.country} {user.delivery?.post_code}
               <br />
               {user.delivery?.city}
               <br />
               {user.delivery?.address}
             </td>
-            <td>{user.telephone_number}</td>
-            <td>
-              <button>Update</button>
-              <button onClick={() => onDelete(user._id)}>Delete</button>
+            <td className="telCol">{user.telephone_number}</td>
+            <td className="btnCol">
+              {expandedRow === user._id ? (
+                <>
+                  <button>Update</button>
+                  <button onClick={() => onDelete(user._id)}>Delete</button>
+                  <button onClick={() => setExpandedRow('')}>-</button>
+                </>
+              ) : (
+                <>
+                  <button onClick={() => setExpandedRow(user._id)}>+</button>
+                </>
+              )}
             </td>
           </tr>
         ))}
