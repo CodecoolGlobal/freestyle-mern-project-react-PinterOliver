d3d016270ba3dd706e97c8294a7424497c83b5d2
Tamas Molnar <125677315+Tomocza@users.noreply.github.com>
2023-07-24 14:06:04 +0200

    style books table style to admin view

--- client/src/components/BooksTable/BooksTable.jsx
+++ client/src/components/BooksTable/BooksTable.jsx
@@ -9,19 +9,19 @@ function BooksTable({ bookList, onDelete }) {
     <table>
       <thead>
         <tr>
-          <th>Title</th>
-          <th>Author</th>
-          <th>Price</th>
-          <th></th>
+          <th className="titleCol">Title</th>
+          <th className="authCol">Author</th>
+          <th className="priceCol">Price</th>
+          <th className="btnCol"></th>
         </tr>
       </thead>
       <tbody>
         {bookList.map((book) => (
           <tr key={book._id}>
-            <td>{book.title}</td>
-            <td>{book.author}</td>
-            <td>{book.price}</td>
-            <td>
+            <td className="titleCol">{book.title}</td>
+            <td className="authCol">{book.author}</td>
+            <td className="priceCol">{book.price.toLocaleString('hu')}</td>
+            <td className="btnCol">
               <button onClick={() => navigate(`update/${book._id}`)}>Update</button>
               <button onClick={() => onDelete(book._id)}>Delete</button>
             </td>
