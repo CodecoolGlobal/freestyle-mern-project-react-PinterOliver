4b0e8563be5bebdc8e312799c6abc2ff93e848b6
Tamas Molnar <125677315+Tomocza@users.noreply.github.com>
2023-07-24 14:31:25 +0200

    add row expansion to orderheaderstable

--- client/src/components/OrderHeadersTable/OrderHeadersTable.jsx
+++ client/src/components/OrderHeadersTable/OrderHeadersTable.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './OrderHeadersTable.css';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 import { useNavigate } from 'react-router';
@@ -6,6 +6,7 @@ import { useNavigate } from 'react-router';
 function OrderHeadersTable({ orderList, onLearnMore, onDelete }) {
   console.log(orderList);
   const navigate = useNavigate();
+  const [expandedRow, setExpandedRow] = useState('');
 
   return (
     <table>
@@ -24,16 +25,24 @@ function OrderHeadersTable({ orderList, onLearnMore, onDelete }) {
             <td>{order.state}</td>
             <td>{formatDistanceToNow(new Date(order.createdAt), { addSuffix: true })}</td>
             <td>
-              <button onClick={() => onLearnMore(order._id)}>Learn more</button>
-            </td>
-            <td>
-              <button onClick={() => onDelete(order._id)}>Delete order</button>
-            </td>
-            <td>
-              <button onClick={() => navigate(`update/${order._id}`)}>Update order state</button>
-            </td>
-            <td>
-              <button onClick={() => navigate(`update/${order._id}`)}>Update order details</button>
+              {expandedRow === order._id ? (
+                <>
+                  {' '}
+                  <button onClick={() => onLearnMore(order._id)}>Learn more</button>
+                  <button onClick={() => onDelete(order._id)}>Delete order</button>
+                  <button onClick={() => navigate(`update/${order._id}`)}>
+                    Update order state
+                  </button>
+                  <button onClick={() => navigate(`update/${order._id}`)}>
+                    Update order details
+                  </button>
+                  <button onClick={() => setExpandedRow('')}>-</button>
+                </>
+              ) : (
+                <>
+                  <button onClick={() => setExpandedRow(order._id)}>+</button>
+                </>
+              )}
             </td>
           </tr>
         ))}
