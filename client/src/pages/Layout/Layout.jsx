93d77fe1a28b8998bd8dac364682d2e31d9221f1
Tamas Molnar <125677315+Tomocza@users.noreply.github.com>
2023-07-26 12:38:25 +0200

    establish websocket connection

--- client/src/pages/Layout/Layout.jsx
+++ client/src/pages/Layout/Layout.jsx
@@ -4,13 +4,23 @@ import { Navigate, Outlet, useNavigate } from 'react-router-dom';
 import NavbarButton from '../../components/NavbarButton/NavbarButton';
 
 function Layout() {
+  const webSocket = new WebSocket('ws://localhost:3000/chat');
+
+  webSocket.onopen = () => {
+    webSocket.send('Websocket connection from client');
+  };
+
+  webSocket.onmessage = (event) => {
+    console.log(event.data);
+  };
+
   const navigate = useNavigate();
 
   const handleLogout = async () => {
     if (window.confirm('Are you sure you want to log out?')) {
       await fetch('/api/login', {
         method: 'DELETE',
-        headers: {token: localStorage.getItem('token')},
+        headers: { token: localStorage.getItem('token') },
       });
       localStorage.removeItem('token');
       localStorage.removeItem('cartid');
@@ -29,19 +39,19 @@ function Layout() {
         <a className="main-logo" href="/">
           <img className="main-logo" src="/icon.png" alt="main logo"></img>
         </a>
-        <a className='topButton' href="/books">
-          <NavbarButton text='Books'/>
+        <a className="topButton" href="/books">
+          <NavbarButton text="Books" />
         </a>
-        <a className='topButton' href="/admin">
-          <NavbarButton text='Admin'/>
+        <a className="topButton" href="/admin">
+          <NavbarButton text="Admin" />
         </a>
-        <a className='topButton' href="/cart">
-          <NavbarButton text='Cart'/>
+        <a className="topButton" href="/cart">
+          <NavbarButton text="Cart" />
         </a>
-        <a className='topButton' href='/presentation'>
-          <NavbarButton text='Presentation'/>
+        <a className="topButton" href="/presentation">
+          <NavbarButton text="Presentation" />
         </a>
-        <NavbarButton onClick={() => handleLogout()} text='Logout'/>
+        <NavbarButton onClick={() => handleLogout()} text="Logout" />
       </div>
       <div className="main-content">
         <Outlet />
--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const ws = require('ws');
 const app = express();
 const apiRouter = require('./routes/api');
+let server;
 
 const { MONGO_URL, PORT = 8080 } = process.env;
 
@@ -17,10 +19,26 @@ app.use('/api', apiRouter);
 const main = async () => {
   await mongoose.connect(MONGO_URL);
 
-  app.listen(PORT, () => {
+  server = app.listen(PORT, () => {
     console.log(`App is listening on ${PORT}`);
   });
+
+  const wss = new ws.WebSocketServer({ server: server, path: '/chat' });
+
+  wss.on('connection', function connection(ws) {
+    ws.on('message', function message(data) {
+      console.log('wss received: %s', data);
+    });
+
+    ws.send('something');
+  });
 };
+
+function getWebSocketResKey(reqKey) {
+  const shasum = crypto.createHash('sha1');
+  const magicString = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
+  return shasum.update(reqKey + magicString).digest('base64');
+}
 
 main().catch((err) => {
   console.error(err);
