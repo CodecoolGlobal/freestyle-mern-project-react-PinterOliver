a8843cba35b80dffe19a93d361b78e9588a966a7
Oliver Peter Pinter <125395118+PinterOliver@users.noreply.github.com>
2023-07-26 15:24:17 +0200

    refactor CartPage.jsx

--- client/src/controllers/fetchOrderItemsController.js
+++ client/src/controllers/fetchOrderItemsController.js
@@ -8,6 +8,38 @@ const fetchGetOrderItems = async (orderHeaderId) => {
   return await moreInfoResponse.json();
 };
 
+const fetchPostOneOrderItem = async (orderHeaderId, orderItem) => {
+  const response = await fetch(`/api/orderitems/orderheaders/${orderHeaderId}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      token: localStorage.getItem('token'),
+    },
+    body: JSON.stringify({
+      bookid: orderItem.id,
+      amount: Number(orderItem.amount),
+    }),
+  });
+  const jsonData = await response.json();
+  jsonData.status = response.status;
+  return jsonData;
+};
+
+const fetchPatchOneOrderItem = async (orderItemId, orderItem) => {
+  const otherData = await fetch(`/api/orderitems/${orderItemId}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      token: localStorage.getItem('token'),
+    },
+    body: JSON.stringify({
+      bookid: orderItem.id,
+      amount: Number(orderItem.amount),
+    }),
+  });
+  return await otherData.json();
+};
+
 const fetchDeleteOneOrderItem = async (orderItemId) => {
   const response = await fetch(`${API_URL}/${orderItemId}`, {headers: {
     token: localStorage.getItem('token'),
@@ -19,5 +51,7 @@ const fetchDeleteOneOrderItem = async (orderItemId) => {
 
 module.exports = {
   fetchGetOrderItems,
+  fetchPostOneOrderItem,
+  fetchPatchOneOrderItem,
   fetchDeleteOneOrderItem,
 };
